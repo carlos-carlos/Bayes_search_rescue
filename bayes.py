9118e152111c82b5485293a6a28c5ca7e510a792
carlos-carlos <carlh5462@fastmail.com>
2020-09-30 16:02:05 -0400

    - Corrected typos and missing characters that were causing crash after the first search

--- bayes.py
+++ bayes.py
@@ -24,18 +24,16 @@ class Search():
     def __init__(self, name):
         self.name = name
         self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
-
-        # check the map file is present
         if self.img is None:
             print(f'Could not load map file {MAP_FILE}',
                   file=sys.stderr)
             sys.exit(1)
 
-        # When the sailor is found. area_actual = search are. sailor_actual = coordinates in the area
+        # Set placeholders for sailor's actual location
         self.area_actual = 0
-        self.sailor_actual = [0, 0] # As "local" coordinates within search area
+        self.sailor_actual = [0, 0]  # As "local" coords within search area
 
-        # Local coordinates within the search areas from 'y' to 'x' upper-left to lower-right
+        # Create numpy arrays for each search area by indexing image array.
         self.sa1 = self.img[SA1_CORNERS[1] : SA1_CORNERS[3],
                             SA1_CORNERS[0] : SA1_CORNERS[2]]
 
@@ -45,12 +43,12 @@ class Search():
         self.sa3 = self.img[SA3_CORNERS[1] : SA3_CORNERS[3],
                             SA3_CORNERS[0] : SA3_CORNERS[2]]
 
-        # Probability of finding the sailor for each area
+        # Set initial per-area target probabilities for finding sailor
         self.p1 = 0.2
         self.p2 = 0.5
         self.p3 = 0.3
 
-        # SEP attributes
+        # Initialize search effectiveness probabilities.
         self.sep1 = 0
         self.sep2 = 0
         self.sep3 = 0
@@ -58,97 +56,87 @@ class Search():
 
     def draw_map(self, last_known):
         """Display basemap with scale, last known xy location, search areas."""
-
+        # Draw the scale bar.
         cv.line(self.img, (20, 370), (70, 370), (0, 0, 0), 2)
         cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
-        cv.putText(self.img, '50 Nautical Miles' , (71, 370),
-            cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        cv.putText(self.img, '50 Nautical Miles', (71, 370),
+                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
 
-        # Draw a rectangle for the first search area
-        cv.rectangle(self.img, (SA1_CORNERS[0], SA1_CORNERS[1]), (SA1_CORNERS[2], SA1_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, '1', (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
-        cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]), (SA2_CORNERS[2], SA2_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, '2', (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
-        cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]), (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, '3', (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        # Draw and number the search areas.
+        cv.rectangle(self.img, (SA1_CORNERS[0], SA1_CORNERS[1]),
+                     (SA1_CORNERS[2], SA1_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '1',
+                   (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15),
+                   cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]),
+                     (SA2_CORNERS[2], SA2_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '2',
+                   (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15),
+                   cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]),
+                     (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '3',
+                   (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15),
+                   cv.FONT_HERSHEY_PLAIN, 1, 0)
 
-        # Post a '+' at the sailor's last known position
-        cv.putText(self.img, '+', (last_known), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        # Post the last known location of the sailor.
+        cv.putText(self.img, '+', (last_known),
+                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, '+ = Last Known Position', (274, 355),
+                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, '* = Actual Position', (275, 370),
+                   cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
 
-        # Text describing symbols for last known position and actual position if found.
-        cv.putText(self.img, '+ = Last Known Position', (274, 355), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
-        cv.putText(self.img, '* = Actual Position', (275, 370), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
-
-        # Show the map with Open CV
-        cv.imshow('Search Area', self.img)  # map title
-        cv.moveWindow('Search Area', 750, 10)  # display the map at the upper left of the monitor
-        cv.waitKey(500)  # delay while rendering images to window in milliseconds
+        cv.imshow('Search Area', self.img)
+        cv.moveWindow('Search Area', 750, 10)
+        cv.waitKey(500)
 
     def sailor_final_location(self, num_search_areas):
         """Return the actual x,y location of the missing sailor."""
         # Find sailor coordinates with respect to any Search Area sub-array.
-        # The coordinates generated by random.choice() will range from 0 to 49.
         self.sailor_actual[0] = np.random.choice(self.sa1.shape[1], 1)
-        self.sailor_actual[1] = np.random.choice(self.sa2.shape[0], 1)
-        #print(np.shape(self.SA1))
+        self.sailor_actual[1] = np.random.choice(self.sa1.shape[0], 1)
 
-        # Pick a search area with the random module to us ethe above coordinates with the full base map image.
+        # Pick a search area at random.
         area = int(random.triangular(1, num_search_areas + 1))
 
+        # Convert local search area coordinates to map coordinates.
         if area == 1:
             x = self.sailor_actual[0] + SA1_CORNERS[0]
-            y = self.sailor_actual[1] + SA2_CORNERS[1]
-
-            # Keep track of the search area by updating self.area_actual attribute
+            y = self.sailor_actual[1] + SA1_CORNERS[1]
             self.area_actual = 1
-
         elif area == 2:
             x = self.sailor_actual[0] + SA2_CORNERS[0]
             y = self.sailor_actual[1] + SA2_CORNERS[1]
             self.area_actual = 2
-
         elif area == 3:
             x = self.sailor_actual[0] + SA3_CORNERS[0]
             y = self.sailor_actual[1] + SA3_CORNERS[1]
             self.area_actual = 3
-
         return x, y
 
     def calc_search_effectiveness(self):
         """Set decimal search effectiveness value per search area."""
-
         self.sep1 = random.uniform(0.2, 0.9)
         self.sep2 = random.uniform(0.2, 0.9)
         self.sep3 = random.uniform(0.2, 0.9)
 
     def conduct_search(self, area_num, area_array, effectiveness_prob):
         """Return search results and list of searched coordinates."""
-
-        # Generate a list of all the coordinates within a given search area
         local_y_range = range(area_array.shape[0])
         local_x_range = range(area_array.shape[1])
-
-        # Generate the list of all coordinates in the search area, use the itertools
         coords = list(itertools.product(local_x_range, local_y_range))
-        # Shuffle the list of coordinates so you won’t keep searching the same
         random.shuffle(coords)
-        # Use index slicing to trim the list based on the search effectiveness probability.
         coords = coords[:int((len(coords) * effectiveness_prob))]
-
-        # Assign a local variable to hold the sailor’s actual location
         loc_actual = (self.sailor_actual[0], self.sailor_actual[1])
-
-        # Use a conditional to check that the sailor has been found
         if area_num == self.area_actual and loc_actual in coords:
-            return f'Found in Area{area_num}', coords
-        else:
-            return 'Not Found', coords
+            return f'Found in Area {area_num}.', coords
+        return 'Not Found', coords
 
     def revise_target_probs(self):
         """Update area target probabilities based on search effectiveness."""
         denom = self.p1 * (1 - self.sep1) + self.p2 * (1 - self.sep2) \
                 + self.p3 * (1 - self.sep3)
-
         self.p1 = self.p1 * (1 - self.sep1) / denom
         self.p2 = self.p2 * (1 - self.sep2) / denom
         self.p3 = self.p3 * (1 - self.sep3) / denom
@@ -171,32 +159,26 @@ def draw_menu(search_num):
     )
 
 def main():
-    '''Runs the program above'''
-
-    # Creates the game application using the Search class
-    app = Search('Cape Python')
-    app.draw_map(last_known=(160, 290)) # Last known x,y position of sailor
+    app = Search('Cape_Python')
+    app.draw_map(last_known=(160, 290))
     sailor_x, sailor_y = app.sailor_final_location(num_search_areas=3)
-    print('-' * 65)
-    print("\nInitial Target (p) Probabilities: ")
+    print("-" * 65)
+    print("\nInitial Target (P) Probabilities:")
     print(f"P1 = {app.p1:.3f}, P2 = {app.p2:.3f}, P3 = {app.p3:.3f}")
-    search_num = 1 # Keeps track of the number of searches
+    search_num = 1
 
-    # Loop runs until user chooses to exit
     while True:
-        app.calc_search_effectiveness()  # Calculate search effectiveness
-        draw_menu(search_num)  # Displays menu and accepts the search number
-        choice = input("Choice: ") # Accept user input
+        app.calc_search_effectiveness()
+        draw_menu(search_num)
+        choice = input("Choice: ")
 
         if choice == "0":
             sys.exit()
 
         elif choice == "1":
-            # Generate 2 sets of results and coordinates
             results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
             results_2, coords_2 = app.conduct_search(1, app.sa1, app.sep1)
-            # Add the two lists above toghether and remove duplicates, divide the pixels in the search area by the set
-            app.sep1 = (len(set(coords_1 + coords_2))) / (len(app.sa1) ** 2)
+            app.sep1 = (len(set(coords_1 + coords_2))) / (len(app.sa1)**2)
             app.sep2 = 0
             app.sep3 = 0
 
@@ -204,7 +186,7 @@ def main():
             results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
             results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
             app.sep1 = 0
-            app.sep2 = (len(set(coords_1 + coords_2))) / (len(app.sa2) ** 2)
+            app.sep2 = (len(set(coords_1 + coords_2))) / (len(app.sa2)**2)
             app.sep3 = 0
 
         elif choice == "3":
@@ -212,9 +194,8 @@ def main():
             results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
             app.sep1 = 0
             app.sep2 = 0
-            app.sep3 = (len(set(coords_1 + coords_2))) / (len(app.sa3) ** 2)
+            app.sep3 = (len(set(coords_1 + coords_2))) / (len(app.sa3)**2)
 
-        #  divide teams between two areas. In this case, there’s no need to recalculate the SEP.
         elif choice == "4":
             results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
             results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
@@ -230,7 +211,6 @@ def main():
             results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
             app.sep1 = 0
 
-        # If the player finds the sailor and wants to play again or just wants to restart, call the main() function
         elif choice == "7":
             main()
 
@@ -238,55 +218,31 @@ def main():
             print("\nSorry, but that isn't a valid choice.", file=sys.stderr)
             continue
 
+        app.revise_target_probs()  # Use Bayes' rule to update target probs.
 
-        app.revise_target_probs() # Use Bayes' rule to update target probs.
-
-        # Display the search results and and search effectiveness probability
-        print(f"\nSearch {search_num} Results 1 = {results_1}", file=sys.stderr)
-        print(f"\nSearch {search_num} Results 2 = {results_2}", file=sys.stderr)
+        print(f"\nSearch {search_num} Results 1 = {results_1}"
+              , file=sys.stderr)
+        print(f"Search {search_num} Results 2 = {results_2}\n"
+              , file=sys.stderr)
         print(f"Search {search_num} Effectiveness (E):")
-        print(f"E1 = {app.sep1:.3f}, E2 = {app.sep2:.3f}, E3 = {app.sep3:.3f}")
+        print(f"E1 = {app.sep1:.3f}, E2 = {app.sep2:.3f}, E3 = {app.sep3:.3f}"
+              )
 
-        # If the results of both searches are negative, display the updated target probabilities
-        if results_1 == 'Not found' and results_2 == 'Not Found':
-            print(f"\nNew Target Probabilities (P) for Search {search_num + 1}:")
-            print(f"P1 = {app.p1:.3f}, P2 = {app.p2:.3f}, P3 = {app.p3:.3f}")
-
-        # Otherwise, display the sailor’s location on the map.
+        # Print target probabilities if sailor not found else show position.
+        if results_1 == 'Not Found' and results_2 == 'Not Found':
+            print(f"\nNew Target Probabilities (P) for Search {search_num + 1}:"
+                  )
+            print(f"P1 = {app.p1:.3f}, P2 = {app.p2:.3f}, P3 = {app.p3:.3f}"
+                  )
         else:
-            cv.circle(app.img, (sailor_x, sailor_y), 3, (255, 0,0), -1)
-            cv.imshow('Search Area', app.img) # Show the basemap
+            cv.circle(app.img, (sailor_x, sailor_y), 3, (255, 0, 0), -1)
+            cv.imshow('Search Area', app.img)
             cv.waitKey(1500)
             main()
         search_num += 1
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
